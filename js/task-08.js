d38b2cdbc713a6206517bd08a67270dd81c25756
Velorian-TDV <terleckydima92@gmail.com>
2023-05-14 17:13:37 +0300

    complete eight task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.login-form');
+const email = loginForm.elements.email;
+const password = loginForm.elements.password;
 
+loginForm.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    if (email.value === '' || password.value === '') {
+        
+        alert('Please fill in all fields');
+        return;
+    
+    }
+
+    const formData = {
+        email: email.value,
+        password: password.value
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+
+})
