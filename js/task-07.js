5daacfa22e7899fd398b9ec00b3cbada03a82056
Velorian-TDV <terleckydima92@gmail.com>
2023-05-14 16:48:42 +0300

    complete seventh task

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,6 @@
 const fontSizeControl = document.getElementById('font-size-control');
 const text = document.getElementById('text');
 
-fontSizeControl.value = 16
-
 fontSizeControl.addEventListener('input', event => {
     text.style.fontSize = `${fontSizeControl.value}px`;
 })
