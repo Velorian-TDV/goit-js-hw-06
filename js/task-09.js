43f981e74d078fd81bfa9eda64cbae39ed4edfdf
Velorian-TDV <terleckydima92@gmail.com>
2023-05-14 17:40:12 +0300

    complete ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const body = document.body;
+const changeColor = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+changeColor.onclick = event => {
+
+  let random = getRandomHexColor();
+
+  color.innerHTML = random;
+  body.style.backgroundColor = random;
+  
 }
